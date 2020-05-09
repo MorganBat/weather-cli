d41a26b3c0db035e9e487df00d5df9bdbdf30e59
Morgan <morgan.batterham@gmail.com>
2020-05-09 22:43:16 +1000

    work on ftp interface

--- nokogiri.rb
+++ nokogiri.rb
@@ -6,10 +6,12 @@ require 'nokogiri'
 # ftp://ftp.bom.gov.au/anon/gen/fwo/IDN11050.xml
 
 forecast = Net::FTP.open('ftp.bom.gov.au') do |ftp|
-    ftp.login
+    ftp.login #('morgan@morganbatterham.me', 'morgan@morganbatterham.me@')
     files = ftp.chdir('anon/gen/fwo')
     files = ftp.list('I*')
-    ftp.getbinaryfile('IDN11050.xml', 1024)
+    ftp.getbinaryfile('IDN11050.xml', blocksize = 1024)
+    puts "success"
 end
 
 p forecast
+# Parse the information with nokogiri
