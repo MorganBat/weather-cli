8835f1b1d520fdf766cfc2bf97f4963bce169716
Morgan <morgan.batterham@gmail.com>
2020-05-10 22:03:20 +1000

    work on accessing data from the XML file

--- nokogiri.rb
+++ nokogiri.rb
@@ -7,7 +7,7 @@ require 'nokogiri'
 
 Net::FTP.open('ftp.bom.gov.au') do |ftp|
     puts "downloading files"
-    ftp.login #('morgan@morganbatterham.me', 'morgan@morganbatterham.me@')
+    ftp.login
     files = ftp.chdir('anon/gen/fwo')
     files = ftp.list('I*')
     @forecast = ftp.gettextfile('IDN11050.xml', nil)
@@ -16,3 +16,16 @@ end
 
 # p @forecast
 # Parse the information with nokogiri
+
+forecast_xml = Nokogiri::XML(@forecast)
+
+text = forecast_xml.xpath("//text")
+
+
+for item in text
+    puts "item is:"
+    p item
+end
+# p forecast_xml.css('')
+
+# product -> forecast -> area aac = "NSW_ME001" description = "Sydney"
